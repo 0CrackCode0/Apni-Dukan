19d0e1a88f21ac07f50118c808e7d84a69d078f1
00Cryptic00 <0Ab.khan0@gmail.com>
2026-04-20 22:50:25 +0530

    fixing errors

--- middlewares/uploader.js
+++ middlewares/uploader.js
@@ -1,19 +1,16 @@
 const multer = require("multer")
-const path = require("path")
 
 function uploader(folder) {
     const storage = multer.diskStorage({
         destination: function (req, file, cb) {
-            cb(null, path.join(__dirname, `../public/uploads/${folder}`))
+            cb(null, `public/uploads/${folder}`)
         },
         filename: function (req, file, cb) {
             cb(null, Date.now() + file.originalname)
         }
     })
-
-    return multer({ storage })
+    return multer({ storage: storage })
 }
-
 module.exports = {
     maincategoryUploader: uploader("maincategory"),
     subcategoryUploader: uploader("subcategory"),
